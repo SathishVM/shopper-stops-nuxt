2d3544ba5d918ed46627679458d94ee41ea7c8fb
SathishVM <sathishvm37@gmail.com>
2023-04-14 11:12:02 +0530

    add error component

---
+++ error.vue
@@ -0,0 +1,33 @@
+<script setup>
+const error = useError()
+
+function handleError() {
+  clearError({ redirect: '/' })
+}
+</script>
+
+<template>
+  <div class="my-10 py-10 md:my-16 md:py-16">
+    <div class="container mx-auto px-4">
+      <div class="text-center">
+        <!-- Icon -->
+        <div class="mb-6 text-6xl">🙁</div>
+
+        <!-- Heading -->
+        <div class="mb-6 font-medium">
+          <h2 v-if="error.statusCode === 404">404. Page not found.</h2>
+          <h2 v-else>{{ error.statusCode }} {{ error.message }}</h2>
+        </div>
+
+        <!-- Text -->
+        <p class="mb-6 text-gray-600">
+          Sorry, we couldn't find the page you where looking for. We suggest
+          that you return to home page.
+        </p>
+
+        <SButton @click="handleError"> Go to Homepage </SButton>
+      </div>
+    </div>
+  </div>
+  <AppFooter />
+</template>
